365ad5197fa70c7ed224a3fc26658c82f45bd74c
createlsworklappy <justinecastillo7@gmail.com>
2026-03-19 15:48:40 +0900

    feat(dashboard): create dashboard route and add test element to view

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -6,4 +6,10 @@ router.get("/", (req, res) => {
   res.send("This route is wired up correctly!");
 });
 
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard", { 
+    title: "Globs - Dashboard"
+  })
+});
+
 export default router;
