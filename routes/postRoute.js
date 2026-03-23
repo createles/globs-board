1f4d6adfcc607af30f6b64504b5d402c4e426ca9
createlsworklappy <justinecastillo7@gmail.com>
2026-03-23 15:35:42 +0900

    feat(postRoute): attach auth middleware and new post GET and POST controllers

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import { isAuth } from "../middleware/authMiddleware.js";
+import { newPostGet, newPostPost } from "../controllers/postController.js";
 
 const router = Router();
 
 router.get("/", (req, res) => {
   res.send("This route is wired up correctly!");
 });
+
+router.get("/new", isAuth, newPostGet);
+router.post("/new", isAuth, newPostPost);
 
 export default router;
