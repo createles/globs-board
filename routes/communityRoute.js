526a468a4eda5a94e72f6ad1535d16df834097d2
createlsworklappy <justinecastillo7@gmail.com>
2026-03-26 11:45:16 +0900

    feat(communities): implement get route to display post from a community

--- routes/communityRoute.js
+++ routes/communityRoute.js
@@ -1,9 +1,33 @@
 import { Router } from "express";
+import * as db from '../db/queries.js';
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("This route is wired up correctly!");
+router.get("/:communityName", async (req, res, next) => {
+  try {
+    const communityName = req.params.communityName;
+    
+    // Check that the community exists
+    const community = await db.getCommunityByName(communityName);
+    
+    // If a user types in a bad URL (e.g., /communities/fakemadesupstuff)
+    if (!community) {
+      return res.status(404).send("404: Community not found!"); 
+    }
+
+    // Fetch posts from the community
+    const posts = await db.getPostsByCommunityId(community.id);
+
+    // 3. Render the community page
+    res.render("community-page", {
+      title: `Globs - g/${community.community_name}`,
+      community: community,
+      posts: posts
+    });
+
+  } catch (err) {
+    next(err);
+  }
 });
 
 export default router;
