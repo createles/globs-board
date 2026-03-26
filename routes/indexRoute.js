cfd191ee4e377e2f1c9228b97fbe9fc68c66b8b7
createlsworklappy <justinecastillo7@gmail.com>
2026-03-26 11:16:11 +0900

    feat(dashboard): create display posts on dashboard logic
    - utilized getAllPosts query to display posts on dashboard global tab
    - created dashboard base layout and styling

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import * as db from '../db/queries.js';
 
 const router = Router();
 
@@ -6,10 +7,17 @@ router.get("/", (req, res) => {
   res.render("splash", { title: "Globs - thoughts and spaces" });
 });
 
-router.get("/dashboard", (req, res) => {
-  res.render("dashboard", { 
-    title: "Globs - Dashboard"
-  })
+router.get("/dashboard", async (req, res, next) => {
+  try {
+    const posts = await db.getAllPosts();
+    
+    res.render("dashboard", { 
+      title: "Globs - Dashboard",
+      posts: posts // Pass the array to the view
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 export default router;
