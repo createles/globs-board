c191167f2013a01043eb6f778902692424c954f8
createlsworklappy <justinecastillo7@gmail.com>
2026-03-26 16:00:09 +0900

    feat(splash): render a couple of posts on the splash page

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -3,18 +3,25 @@ import * as db from '../db/queries.js';
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.render("splash", { title: "Globs - thoughts and spaces" });
+// Update the root route to be async and fetch posts
+router.get("/", async (req, res, next) => {
+  try {
+    const posts = await db.getAllPosts();
+    
+    res.render("splash", { 
+      title: "Globs - blips and thoughts",
+      // Pass a couple of posts to splash page
+      posts: posts.slice(0, 3) 
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get("/dashboard", async (req, res, next) => {
   try {
     const posts = await db.getAllPosts();
-    
-    res.render("dashboard", { 
-      title: "Globs - Dashboard",
-      posts: posts // Pass the array to the view
-    });
+    res.render("dashboard", { title: "Globs - Dashboard", posts: posts });
   } catch (err) {
     next(err);
   }
